05f9ee20ff4bc7ed7cb36a4fe19d6ff8c2f85e56
Marek Piskorek <marek.piskorek@gmail.com>
2018-10-14 09:53:08 +0200

    OMDb API client added

--- rzepa/movies/tests.py
+++ rzepa/movies/tests.py
@@ -1,18 +1,15 @@
-from collections import OrderedDict
-
 import pytest
 
 from movies.models import Movie
 
 
+@pytest.mark.django_db
 class TestMoviesEndpoint:
-    @pytest.mark.django_db
     def test_get_on_empty_database_returns_empty_list(self, client):
         response = client.get("/movies/")
         assert response.status_code == 200
         assert response.data == []
 
-    @pytest.mark.django_db
     def test_get_all_exsisting_movies(self, client):
         movie_1 = Movie.objects.create(title="Godfather")
         movie_2 = Movie.objects.create(title="Gone With The Wind")
@@ -20,19 +17,17 @@ class TestMoviesEndpoint:
         response = client.get("/movies/")
         assert response.status_code == 200
         response_movie_1, response_movie_2, response_movie_3 = sorted(
-            response.data, key=lambda k: k["title"]
+            response.data, key=lambda k: k["Title"]
         )
-        assert response_movie_1["title"] == movie_1.title
-        assert response_movie_2["title"] == movie_2.title
-        assert response_movie_3["title"] == movie_3.title
+        assert response_movie_1["Title"] == movie_1.title
+        assert response_movie_2["Title"] == movie_2.title
+        assert response_movie_3["Title"] == movie_3.title
 
-    @pytest.mark.django_db
     def test_post_without_title_fails(self, client):
         response = client.post("/movies/", {})
         assert response.status_code == 400
 
-    @pytest.mark.django_db
     def test_post_with_title_returns_created_movie_data(self, client):
         response = client.post("/movies/", {"title": "Citizen Kane"})
         assert response.status_code == 201
-        assert response.data["title"] == "Citizen Kane"
+        assert response.data["Title"] == "Citizen Kane"
